e9f46db98763b963a52f2045280e8dc7c4b10eac
yaoxinlong <1032500810@qq.com>
2019-02-15 15:42:30 +0800

    后台首页开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
+import User from '@/components/User'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User }
+      ]
+    }
   ]
 })
 
